2fc9462b8f1450c40e2100640dce220aa6ced5f0
tsubasa-python <tsubasaalexkao@gmail.com>
2024-09-20 20:36:28 +0800

    class2

--- class2/class.py
+++ class2/class.py
@@ -1,1 +1,28 @@
-print("hello")
+# 文字列
+a = "hello "
+b = "world "
+c = a + b  # これは、aとbを結合した文字列を作る
+print(c)
+a = a * 3  # これは、aの文字列を3倍にする
+print(a)
+
+
+m = max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1234567890)  # 最大値を探す
+print(m)  # 最大値を返す
+mi = min(3, 6, 32, 38, 2)  # 最小値を探す
+print(mi)  # 最小値を返す
+
+l = len("hello")  # 文字列の長さを求める
+print(l)  # 文字列の長さを返す
+
+t = 3  # これは、整数を表す。
+print(t)
+t2 = 3.0  # これは、小数を表す。
+print(t2)
+t3 = "hello"  # これは、文字列を表す。
+print(t3)
+
+print(type(1))  # これは,型を表す。
+print(type("hello"))  # これは,型を表す。
+print(type(3.0))  # これは,型を表す。
+print(type(True))  # これは,型を表す。
