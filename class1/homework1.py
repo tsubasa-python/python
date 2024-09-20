9280a6ec55ae5e4e98cf39d816b275e0ff9e3bfe
tsubasa-python <tsubasaalexkao@gmail.com>
2024-09-20 20:08:05 +0800

    mesage correct1 homework1

---
+++ class1/homework1.py
@@ -0,0 +1,13 @@
+"""
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+
+int = apple ,age #int=せいすう
+bool = minor #bool=  真偽値
+str = name zip #str=文字列
+float = weight #float=小数
+"""
---
+++ class1/prj1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+# basic types
+print(1)  # 整数　int
+print(1.0)  # 小数　float
+print("hello")  # 文字列　str
+print(True)  #  真偽値　bool
+print(None)  # なし　None
+variable = 1  # 変数　variable
+a = 1  # 変数　a
+# プログラミングでの＝は、左の数値を右の数値に代入する。
