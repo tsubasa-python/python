1d6467d1d91042d62f7da1d814ac31f00ee41b39
tsubasa-python <tsubasaalexkao@gmail.com>
2024-11-08 19:28:33 +0800

    homework8

--- class8/homework8.py
+++ class8/homework8.py
@@ -24,3 +24,17 @@ Result:
 
 In this example, the program finds and lists all prime numbers between 10 and 50.
 """
+
+start = int(input("Please enter the starting number: "))
+end = int(input("Please enter the ending number: "))
+for num in range(start, end + 1):
+    # num = int(input("Enter a number: "))
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+
+    if is_prime and num > 1:
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
